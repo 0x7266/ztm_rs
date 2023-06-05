3480d6de56cd1ec89ecb11b47b8dca7fd9862666
unknown <rrrr0ffff@gmail.com>
2023-06-04 23:47:10 -0300

    a7

---
+++ activities/a7.rs
@@ -0,0 +1,26 @@
+// Topic: Working with an enum
+//
+// Program requirements:
+// * Prints the name of a color to the terminal
+//
+// Notes:
+// * Use an enum with color names as variants
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+// * Use a match expression to determine which color
+//   name to print
+
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+fn main() {
+    let color = Color::Blue;
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+    };
+}
