b3992894e89491edab985b900c50cf31bfa4377b
unknown <rrrr0ffff@gmail.com>
2023-06-06 15:01:11 -0300

    a15

---
+++ activities/a15.rs
@@ -0,0 +1,34 @@
+// Topic: Advanced match
+//
+// Requirements:
+// * Print out a list of tickets and their information for an event
+// * Tickets can be Backstage, Vip, and Standard
+// * Backstage and Vip tickets include the ticket holder's name
+// * All tickets include the price
+//
+// Notes:
+// * Use an enum for the tickets with data associated with each variant
+// * Create one of each ticket and place into a vector
+// * Use a match expression while iterating the vector to print the ticket info
+
+enum Ticket {
+    Backstage(f32, String),
+    Vip(f32, String),
+    Standard(f32),
+}
+
+fn main() {
+    let b_ticket = Ticket::Backstage(200.0, String::from("John"));
+    let v_ticket = Ticket::Vip(150.0, String::from("Mary"));
+    let s_ticket = Ticket::Standard(80.0);
+
+    let tickets = vec![b_ticket, v_ticket, s_ticket];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, name) => println!("PRICE: {}\nNAME: {}\n\n", price, name),
+            Ticket::Vip(price, name) => println!("PRICE: {}\nNAME: {}\n\n", price, name),
+            Ticket::Standard(price) => println!("PRICE: {}\n\n", price),
+        }
+    }
+}
