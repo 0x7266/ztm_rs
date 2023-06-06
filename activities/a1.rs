a2a32020ff352f27ad560e2ccbbaee01c33030d5
unknown <rrrr0ffff@gmail.com>
2023-06-06 15:07:14 -0300

    removed question/doubt

--- activities/a1.rs
+++ activities/a1.rs
@@ -13,7 +13,11 @@ fn main() {
     let last_name = "Doe";
     print_first_name(first_name);
     print_last_name(last_name);
-    println!("Full name: {} {}", print_first_name(&first_name), print_last_name(&last_name));
+    println!(
+        "Full name: {} {}",
+        print_first_name(&first_name),
+        print_last_name(&last_name)
+    );
 }
 
 fn print_first_name(first_name: &str) -> String {
@@ -25,6 +29,3 @@ fn print_last_name(last_name: &str) -> String {
     println!("{}", last_name);
     String::from(last_name)
 }
-
-
-// print_first_name() and print_last_name() return a String. since i'm not storing it in a variable, does it instatly get dropped or does it only get dropped when main() finishes?
