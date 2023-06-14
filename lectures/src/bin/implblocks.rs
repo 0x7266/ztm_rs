c7b11752f76635d9522c20c545d895a8949b7cdb
unknown <rrrr0ffff@gmail.com>
2023-06-14 01:11:52 -0300

    implblocks

---
+++ lectures/src/bin/implblocks.rs
@@ -0,0 +1,53 @@
+trait Game {
+    fn name(&self) -> String;
+}
+
+#[derive(Debug)]
+enum BoardGame {
+    Chess,
+    Monopoly,
+}
+impl Game for BoardGame {
+    fn name(&self) -> String {
+        match self {
+            BoardGame::Chess => String::from("Chess"),
+            BoardGame::Monopoly => String::from("Monopoly"),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum VideoGame {
+    PlayStation,
+    Xbox,
+}
+impl Game for VideoGame {
+    fn name(&self) -> String {
+        match self {
+            Self::PlayStation => String::from("PlayStation"),
+            Self::Xbox => String::from("Xbox"),
+        }
+    }
+}
+
+struct PlayRoom<T: Game> {
+    game: T,
+}
+impl<T: Game> PlayRoom<T> {
+    fn game_info(&self) {
+        println!("{}", self.game.name());
+    }
+}
+
+fn main() {
+    let board_game = PlayRoom {
+        game: BoardGame::Chess,
+    };
+    let video_game = PlayRoom {
+        game: VideoGame::PlayStation,
+    };
+    board_game.game_info();
+    board_game.game.name();
+    video_game.game_info();
+    video_game.game.name();
+}
