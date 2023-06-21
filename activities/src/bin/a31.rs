75246f4d9e1ece3b2745f00246b68c6cefd757f6
unknown <rrrr0ffff@gmail.com>
2023-06-21 00:59:47 -0300

    a31

--- activities/src/bin/a31.rs
+++ activities/src/bin/a31.rs
@@ -23,40 +23,78 @@ trait Cost {
     fn get_cost(&self) -> f64;
 }
 
-enum Material {
-    Carpet(f64),
-    Tile(f64),
-    Wood(f64),
-}
-
-impl Cost for Material {
+struct Carpet(f64);
+impl Cost for Carpet {
     fn get_cost(&self) -> f64 {
-        match self {
-            Self::Carpet(square_meters) => 10. * square_meters,
-            Self::Tile(square_meters) => 15. * square_meters,
-            Self::Wood(square_meters) => 20. * square_meters,
-        }
+        self.0 * 10.
+    }
+}
+struct Tile(f64);
+impl Cost for Tile {
+    fn get_cost(&self) -> f64 {
+        self.0 * 15.
+    }
+}
+struct Wood(f64);
+impl Cost for Wood {
+    fn get_cost(&self) -> f64 {
+        self.0 * 20.
     }
 }
 
 fn calculate_cost(order: Vec<Box<dyn Cost>>) -> f64 {
     order.iter().map(|product| product.get_cost()).sum()
 }
-// fn calculate_cost(order: Vec<Box<Material>>) -> f64 {
-//     order.iter().map(|product| product.get_cost()).sum()
-// }
-// fn calculate_cost(order: Vec<Material>) -> f64 {
-//     order.iter().map(|product| product.get_cost()).sum()
-// }
 
 fn main() {
-    let carpet = Box::new(Material::Carpet(40.));
-    let tile = Box::new(Material::Tile(60.));
-    let wood = Box::new(Material::Wood(100.));
+    let carpet = Box::new(Carpet(40.));
+    let tile = Box::new(Tile(60.));
+    let wood = Box::new(Wood(100.));
 
     let order: Vec<Box<dyn Cost>> = vec![carpet, tile, wood];
-    // let order: Vec<Material> = vec![*carpet, *tile, *wood];
-    // let order: Vec<Box<Material>> = vec![carpet, tile, wood];
 
     println!("{}", calculate_cost(order));
 }
+
+// PLAYING AROUND WITH ENUMS
+// trait Cost {
+//     fn get_cost(&self) -> f64;
+// }
+
+// enum Material {
+//     Carpet(f64),
+//     Tile(f64),
+//     Wood(f64),
+// }
+
+// impl Cost for Material {
+//     fn get_cost(&self) -> f64 {
+//         match self {
+//             Self::Carpet(square_meters) => 10. * square_meters,
+//             Self::Tile(square_meters) => 15. * square_meters,
+//             Self::Wood(square_meters) => 20. * square_meters,
+//         }
+//     }
+// }
+
+// fn calculate_cost(order: Vec<Box<dyn Cost>>) -> f64 {
+//     order.iter().map(|product| product.get_cost()).sum()
+// }
+// // fn calculate_cost(order: Vec<Box<Material>>) -> f64 {
+// //     order.iter().map(|product| product.get_cost()).sum()
+// // }
+// // fn calculate_cost(order: Vec<Material>) -> f64 {
+// //     order.iter().map(|product| product.get_cost()).sum()
+// // }
+
+// fn main() {
+//     let carpet = Box::new(Material::Carpet(40.));
+//     let tile = Box::new(Material::Tile(60.));
+//     let wood = Box::new(Material::Wood(100.));
+
+//     let order: Vec<Box<dyn Cost>> = vec![carpet, tile, wood];
+//     // let order: Vec<Material> = vec![*carpet, *tile, *wood];
+//     // let order: Vec<Box<Material>> = vec![carpet, tile, wood];
+
+//     println!("{}", calculate_cost(order));
+// }
