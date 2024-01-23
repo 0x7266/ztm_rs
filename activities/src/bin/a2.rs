f46be50a29916aa7858683487e7e4c083d9ca24c
rafael <rrrr0ffff@gmail.com>
2024-01-23 00:22:05 -0300

    added test

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -9,13 +9,46 @@
 // * Use the "{:?}" token in the println macro to display the result
 
 fn main() {
-    sum(3, 4);
+    let mut logger = ConsoleLogger;
+    sum(3, 4, &mut logger);
 }
 
-fn sum(a: i32, b:i32){
-    print_result(a + b);
+fn sum(a: i32, b: i32, logger: &mut dyn Logger) {
+    print_result(a + b, logger);
 }
 
-fn print_result(res: i32) {
-    println!("{}", res);
+fn print_result(res: i32, logger: &mut dyn Logger) {
+    logger.log(res.to_string());
+}
+
+trait Logger {
+    fn log(&mut self, value: String);
+}
+
+struct ConsoleLogger;
+
+impl Logger for ConsoleLogger {
+    fn log(&mut self, value: String) {
+        println!("{}", value);
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Default)]
+    struct TestLogger(Vec<String>);
+
+    impl Logger for TestLogger {
+        fn log(&mut self, value: String) {
+            self.0.push(value)
+        }
+    }
+
+    #[test]
+    fn test_sum() {
+        let mut test_logger = TestLogger::default();
+        sum(10, 20, &mut test_logger);
+        assert_eq!(String::from("30"), test_logger.0[0]);
+    }
 }
