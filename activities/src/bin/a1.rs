30c3d8f78185d6fa866c46c3286724face0da818
rafael <rrrr0ffff@gmail.com>
2024-01-23 11:14:15 -0300

    changed to custom logger

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -9,49 +9,28 @@
 // * Use the println macro to display messages to the terminal
 
 fn main() {
-    let mut logger = ConsoleLogger;
+    let mut logger = utils::logger::ConsoleLogger;
     let first_name = "John";
     let last_name = "Doe";
     print_first_name(first_name, &mut logger);
     print_last_name(last_name, &mut logger);
 }
 
-fn print_first_name(first_name: &str, logger: &mut dyn Logger) {
+fn print_first_name(first_name: &str, logger: &mut dyn utils::logger::Logger) {
     logger.log(first_name.to_string());
 }
 
-fn print_last_name(last_name: &str, logger: &mut dyn Logger) {
+fn print_last_name(last_name: &str, logger: &mut dyn utils::logger::Logger) {
     logger.log(last_name.to_string());
-}
-
-trait Logger {
-    fn log(&mut self, value: String);
-}
-
-struct ConsoleLogger;
-
-impl Logger for ConsoleLogger {
-    fn log(&mut self, value: String) {
-        println!("{}", value);
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[derive(Default)]
-    struct TestLogger(Vec<String>);
-
-    impl Logger for TestLogger {
-        fn log(&mut self, value: String) {
-            self.0.push(value);
-        }
-    }
-
     #[test]
     fn print_first_name_test() {
-        let mut test_logger = TestLogger::default();
+        let mut test_logger = utils::logger::test::TestLogger::default();
         print_first_name("John", &mut test_logger);
         assert_eq!(String::from("John"), test_logger.0[0]);
         print_last_name("Doe", &mut test_logger);
--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -9,45 +9,25 @@
 // * Use the "{:?}" token in the println macro to display the result
 
 fn main() {
-    let mut logger = ConsoleLogger;
+    let mut logger = utils::logger::ConsoleLogger;
     sum(3, 4, &mut logger);
 }
 
-fn sum(a: i32, b: i32, logger: &mut dyn Logger) {
+fn sum(a: i32, b: i32, logger: &mut dyn utils::logger::Logger) {
     print_result(a + b, logger);
 }
 
-fn print_result(res: i32, logger: &mut dyn Logger) {
+fn print_result(res: i32, logger: &mut dyn utils::logger::Logger) {
     logger.log(res.to_string());
 }
 
-trait Logger {
-    fn log(&mut self, value: String);
-}
-
-struct ConsoleLogger;
-
-impl Logger for ConsoleLogger {
-    fn log(&mut self, value: String) {
-        println!("{}", value);
-    }
-}
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[derive(Default)]
-    struct TestLogger(Vec<String>);
-
-    impl Logger for TestLogger {
-        fn log(&mut self, value: String) {
-            self.0.push(value)
-        }
-    }
-
     #[test]
     fn test_sum() {
-        let mut test_logger = TestLogger::default();
+        let mut test_logger = utils::logger::test::TestLogger::default();
         sum(10, 20, &mut test_logger);
         assert_eq!(String::from("30"), test_logger.0[0]);
     }
